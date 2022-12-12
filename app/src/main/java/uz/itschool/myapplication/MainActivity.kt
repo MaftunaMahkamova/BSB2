7b2a44910d4838526a9013316774f23206520f6a
PC672 <mahkamovamaftuna2007@gmail.com>
2022-12-12 15:36:36 +0500

    add calc

---
+++ app/src/main/java/uz/itschool/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.itschool.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.itschool.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
